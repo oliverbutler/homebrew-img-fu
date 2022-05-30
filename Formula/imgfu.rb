b28a32841e7f5c62369d31eb626fa1980e3b3265
Oliver Butler <dev@oliverbutler.uk>
2022-05-30 22:36:20 +0100

    Fix hash

--- Formula/imgfu.rb
+++ Formula/imgfu.rb
@@ -5,7 +5,7 @@ class Imgfu < Formula
   desc "A steganography tool written in Rust"
   homepage "https://github.com/oliverbutler/imgfu"
   url "https://github.com/oliverbutler/imgfu/releases/download/release/imgfu.tar.gz"
-  sha256 "f05565267ffc700f8e221ea1d58c2ab610df9031"
+  sha256 "4c10532c10e9f3558e33172f5840ff70c4495b90a8b4fe32f953ae7b9449ef53"
   version "1.0.0"
 
   def install
