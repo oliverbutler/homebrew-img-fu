c8a3852e32a2a4f4de97df281e778cd5a20041e5
Oliver Butler <dev@oliverbutler.uk>
2022-05-30 22:42:52 +0100

    rename to steg

--- Formula/steg.rb
+++ Formula/steg.rb
@@ -1,14 +1,14 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Imgfu < Formula
+class Steg < Formula
   desc "A steganography tool written in Rust"
-  homepage "https://github.com/oliverbutler/imgfu"
-  url "https://github.com/oliverbutler/imgfu/releases/download/release/imgfu.tar.gz"
-  sha256 "4c10532c10e9f3558e33172f5840ff70c4495b90a8b4fe32f953ae7b9449ef53"
+  homepage "https://github.com/oliverbutler/steg"
+  url "https://github.com/oliverbutler/steg/releases/download/release/steg.tar.gz"
+  sha256 "06c63e18b81d15a89b6c89817038174b27ad78e42bdc1df6f4dcebdf9d0dff58"
   version "1.0.0"
 
   def install
-    bin.install "imgfu"
+    bin.install "steg"
   end
 end
