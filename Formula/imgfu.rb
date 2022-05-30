bad8afd637b0be002f2f75e3d9eca67090945c3a
Oliver Butler <dev@oliverbutler.uk>
2022-05-30 22:24:13 +0100

    Spelling

--- Formula/imgfu.rb
+++ Formula/imgfu.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Imgfu < Formula
   desc "A steganography tool written in Rust"
-  homepage "https://github.com/oliverbutler/img-fu"
-  url "https://github.com/oliverbutler/img-fu/releases/download/release/main.tar.gz"
+  homepage "https://github.com/oliverbutler/imgfu"
+  url "https://github.com/oliverbutler/imgfu/releases/download/release/main.tar.gz"
   sha256 "2f8739a0ac12255a7ff4dcbcafe91ccd4bf8465ebd214ab718f6354a55f2bc51"
   version "1.0.0"
 
