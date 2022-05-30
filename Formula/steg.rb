12d11e70b88c4c3ef9be2fa5557af7891e8d77e9
Oliver Butler <dev@oliverbutler.uk>
2022-05-30 22:43:27 +0100

    fix hash

--- Formula/steg.rb
+++ Formula/steg.rb
@@ -5,7 +5,7 @@ class Steg < Formula
   desc "A steganography tool written in Rust"
   homepage "https://github.com/oliverbutler/steg"
   url "https://github.com/oliverbutler/steg/releases/download/release/steg.tar.gz"
-  sha256 "06c63e18b81d15a89b6c89817038174b27ad78e42bdc1df6f4dcebdf9d0dff58"
+  sha256 "0d058d94e159a22c4adf45ab2b9e1f81a9b7b39aca84b59404c2a0118842b11c"
   version "1.0.0"
 
   def install
