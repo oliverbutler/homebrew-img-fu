5aacf85c3b77bc85f01ef6cd0c3aabbd278138de
Oliver Butler <dev@oliverbutler.uk>
2022-06-01 23:46:18 +0100

    Update hash

--- Formula/steggy.rb
+++ Formula/steggy.rb
@@ -5,7 +5,7 @@ class Steggy < Formula
   desc "A steganography tool written in Rust"
   homepage "https://github.com/oliverbutler/steggy"
   url "https://github.com/oliverbutler/steggy/releases/download/release/steggy.tar.gz"
-  sha256 "0d058d94e159a22c4adf45ab2b9e1f81a9b7b39aca84b59404c2a0118842b11c"
+  sha256 "3da5d74503e0f3dcb68fdcd6ac2218875368e8a08ca78f47685b0be0133bdc79"
   version "1.0.0"
 
   def install
