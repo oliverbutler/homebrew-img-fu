48496ddb2a37dede97b4eb2b931808aa95fb86a6
Oliver Butler <dev@oliverbutler.uk>
2022-06-01 23:45:42 +0100

    Change steggy path

--- Formula/steggy.rb
+++ Formula/steggy.rb
@@ -4,7 +4,7 @@
 class Steggy < Formula
   desc "A steganography tool written in Rust"
   homepage "https://github.com/oliverbutler/steggy"
-  url "https://github.com/oliverbutler/steggy/releases/download/release/steg.tar.gz"
+  url "https://github.com/oliverbutler/steggy/releases/download/release/steggy.tar.gz"
   sha256 "0d058d94e159a22c4adf45ab2b9e1f81a9b7b39aca84b59404c2a0118842b11c"
   version "1.0.0"
 
